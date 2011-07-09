b29fe4b164dcc40a703aad9e92217b931ad808ae
Mathieu Virbel <mat@kivy.org>
2011-07-09 19:38:57 +0200

    update slides and add plain white video

--- js/kivy.js
+++ js/kivy.js
@@ -8,7 +8,30 @@ $(document).ready(function () {
 		preloadImage: 'img/loading.gif',
 		play: 5000,
 		pause: 10000000,
-		hoverPause: true
+		hoverPause: true,
+	    animationStart: function(current){
+			$('.caption').animate({
+				top:-35
+			},100);
+			if (window.console && console.log) {
+				// example return of current slide number
+				console.log('animationStart on slide: ', current);
+			};
+		},
+		animationComplete: function(current){
+			$('.caption').animate({
+				top:0
+			},200);
+			if (window.console && console.log) {
+				// example return of current slide number
+				console.log('animationComplete on slide: ', current);
+			};
+		},
+		slidesLoaded: function() {
+			$('.caption').animate({
+				top:0
+			},200);
+		}
 	});
 
 	//
